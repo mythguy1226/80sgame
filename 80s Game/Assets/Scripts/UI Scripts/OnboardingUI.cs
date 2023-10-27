5e523fa5c1278d4496bbda2f7c87dc9ca18e0511
Kevin-Insinna <kji8379@rit.edu>
2023-10-27 17:28:06 -0400

    Disabled pausing during onboarding and renamed game object with joycon manager script to JoyConManager for clarity

--- 80s Game/Assets/Scripts/UI Scripts/OnboardingUI.cs
+++ 80s Game/Assets/Scripts/UI Scripts/OnboardingUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OnboardingUI : MonoBehaviour
 {
     public GameObject onboardingPanel;
+    public GameObject gameUIElements;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,8 @@ public class OnboardingUI : MonoBehaviour
     public void CloseOnboarding()
     {
         onboardingPanel.SetActive(false);
+        gameUIElements.SetActive(true);
+
         Time.timeScale = 1.0f;
     }
 }
