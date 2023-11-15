1dca439c534d360a801246bdb7259821ea68adbd
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2023-11-15 00:36:19 -0500

    Basic score writing and reading

--- 80s Game/Assets/Scripts/PointsManager.cs
+++ 80s Game/Assets/Scripts/PointsManager.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class PointsManager : MonoBehaviour
 {
     // Public property
-    public int Points {  get; private set; }
+    public int Points { get; private set; }
     public int RoundPoints { get; private set; }
+    private string filePath = Application.dataPath + "/PlayerData/scores.txt";
+    private int numTopScores = 6;
 
     // Start is called before the first frame update
     void Awake()
@@ -46,5 +51,62 @@ public class PointsManager : MonoBehaviour
         Points += RoundPoints;
         RoundPoints = 0;
         return Points;
+    }
+
+    /// <summary>
+    /// Loads the scores from "scores.txt"
+    /// </summary>
+    /// <returns>A list of integers parsed from the data in "scores.txt"</returns>
+    public List<int> LoadScores()
+    {
+        if(File.Exists(filePath))
+        {
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            List<int> scores = new List<int>();
+            // parse each top line and add it to the top scores
+            foreach(string line in lines)
+            {
+                if (int.TryParse(line, out int score))
+                {
+                    scores.Add(score);
+                }
+                else
+                {
+                    Debug.LogWarning("Failed to parse score: " + line);
+                }
+            }
+
+            // convert list to array
+            return scores;
+        }
+        else
+        {
+            Debug.LogWarning("Scores file not found!");
+            return new List<int> { };
+        }
+    }
+
+    /// <summary>
+    /// Saves the current overal score to "scores.txt"
+    /// </summary>
+    public void SaveScore()
+    {
+        string dirPath = Path.GetDirectoryName(filePath);
+        // Create the file if it doesn't exist
+        if(!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+
+        // Write to the file
+        using(StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(Points);
+        }
+
+        // Print the new set of scores
+        LoadScores().ForEach(score => Debug.Log(score));
     }
 }
--- 80s Game/Assets/Scripts/Target.cs
+++ 80s Game/Assets/Scripts/Target.cs
@@ -179,6 +179,7 @@ public class Target : MonoBehaviour
             if (GameManager.Instance.TargetManager.currentRound == GameManager.Instance.TargetManager.numRounds)
             {
                 GameManager.Instance.TargetManager.gameOver = true;
+                GameManager.Instance.PointsManager.SaveScore();
                 return;
             }
 
