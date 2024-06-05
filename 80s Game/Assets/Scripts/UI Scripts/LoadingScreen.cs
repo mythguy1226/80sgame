e494ff333d52de82978bae4615ee73fe985c4fb6
Kevin-Insinna <kji8379@rit.edu>
2024-06-05 15:50:46 -0400

    Loading Screen functional

--- 80s Game/Assets/Scripts/UI Scripts/LoadingScreen.cs
+++ 80s Game/Assets/Scripts/UI Scripts/LoadingScreen.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
 
 public class LoadingScreen : MonoBehaviour
 {
     public GameObject loadingScreen;
+    public GameObject controllerDiagram;
+    public GameObject mouseDiagram;
     public Image loadingBarFill;
 
-    public void LoadScene(int sceneId)
+    public GameObject startButton;
+    public GameObject startButtonPrompt;
+    public Sprite controllerConfirmIcon;
+    public Sprite mouseConfirmIcon;
+
+    private bool confirmSceneLoad = false;
+
+    public void LoadScene(int sceneId, bool controller)
     {
-        StartCoroutine(LoadSceneAsync(sceneId));
+        StartCoroutine(LoadSceneAsync(sceneId, controller));
     }
 
-    IEnumerator LoadSceneAsync(int sceneId)
+    IEnumerator LoadSceneAsync(int sceneId, bool controller)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        operation.allowSceneActivation = confirmSceneLoad;
 
         loadingScreen.SetActive(true);
+
+        if (!controller)
+        {
+            mouseDiagram.SetActive(true);
+            startButtonPrompt.GetComponent<Image>().sprite = mouseConfirmIcon;
+        }
+
+        else
+        {
+            controllerDiagram.SetActive(true);
+            startButtonPrompt.GetComponent<Image>().sprite = controllerConfirmIcon;
+        }
 
         while (!operation.isDone)
         {
@@ -26,7 +50,23 @@ public class LoadingScreen : MonoBehaviour
 
             loadingBarFill.fillAmount = progressValue;
 
+            if (operation.progress >= 0.9f)
+            {
+                startButton.GetComponent<TextMeshProUGUI>().text = "Start";
+                startButtonPrompt.SetActive(true);
+
+                EventSystem.current.SetSelectedGameObject(null);
+                EventSystem.current.SetSelectedGameObject(startButton);
+
+                startButton.GetComponent<Button>().onClick.AddListener(() => operation.allowSceneActivation = true);
+            }
+
             yield return null;
         }
+    }
+
+    public void ConfirmStart()
+    {
+        confirmSceneLoad = true;
     }
 }
