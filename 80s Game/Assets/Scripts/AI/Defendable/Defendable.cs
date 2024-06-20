c86748efe06734ccac8c65a30b466cf9c366b854
amonjerro <eescudero_arens@hotmail.com>
2024-06-20 11:11:39 -0400

    Groundwork for defendable object

--- 80s Game/Assets/Scripts/AI/Defendable/Defendable.cs
+++ 80s Game/Assets/Scripts/AI/Defendable/Defendable.cs
@@ -3,38 +3,75 @@ using UnityEngine;
 
 public class Defendable : MonoBehaviour
 {
-
+    // The class that governs a defendable object's behavior
+    // This object has a several child transforms object whose transform are used as latch points for targets
+    // It also has a particle system and an array of sprites that help give feedback to the player
+    // on levels of damage
+    // Lastly, an optional field for a health bar is included
     [SerializeField]
     int _maxHitpoints;
 
-    [Tooltip("Hp Thresholds to switch to the next sprite. Add these in decreasing order")]
+    [Tooltip("Hp Thresholds to switch to the next sprite. Add these in decreasing order.")]
     [SerializeField]
     List<float> hpThresholds;
-
+    
+    [Tooltip("Sprites to use for different levels of damage")]
     [SerializeField]
     List<Sprite> damageLevelSprites;
+
+    [Tooltip("The hp-threshold index from which the particle system should start spawning smoke")]
+    [SerializeField]
+    int _smokeIndex;
+
+    [Tooltip("Particles for showing a smoke effect at a certain HP threshold")]
+    [SerializeField]
+    ParticleSystem smokeSystem;
 
     int _currentHitpoints;
     int _thresholdIndex;
 
     List<LatchPoint> latchPoints;
     SpriteRenderer sr;
+    HealthBar healthbar;
+
+
+    [HideInInspector]
+    public bool bCanBeTargeted;
 
     private void Start()
     {
+        bCanBeTargeted = true;
         sr = GetComponent<SpriteRenderer>();
+        smokeSystem.Stop();
         latchPoints = new List<LatchPoint>();
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
             latchPoints.Add(child.GetComponent<LatchPoint>());
         }
         _thresholdIndex = 0;
+
         _currentHitpoints = _maxHitpoints;
+        if (healthbar != null)
+        {
+            healthbar.Config(_maxHitpoints);
+        }
     }
 
+    /// <summary>
+    /// Have this defendable object take some damage
+    /// </summary>
+    /// <param name="damage">The amount of damage to receive</param>
     public void TakeDamage(int damage)
     {
         _currentHitpoints -= damage;
+        if (_currentHitpoints <= 0)
+        {
+            bCanBeTargeted = false;
+        }
+        if (healthbar != null)
+        {
+            healthbar.DecreaseValue(damage);
+        }
         for(int i = _thresholdIndex + 1;  i < hpThresholds.Count; i++)
         {
             if (_currentHitpoints < hpThresholds[i])
@@ -43,17 +80,31 @@ public class Defendable : MonoBehaviour
                 UpdateSprite();
             }
         }
+
+        // Start the smoke emission
+        if (_thresholdIndex >= _smokeIndex && !smokeSystem.isEmitting)
+        {
+            smokeSystem.Play();
+        }
     }
 
+    /// <summary>
+    /// Update the sprite to the threshold index
+    /// </summary>
     private void UpdateSprite()
     {
+        // Immediately change the sprite to the next 
         sr.sprite = damageLevelSprites[_thresholdIndex];
+
+        // We might want to extend this change with an animation and some sound
     }
 
+
+    // Bats that collide with the latch radius should be offered a latch point, if one exists
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Filter out non-enemies
-        if (!collision.gameObject.CompareTag("Enemy"))
+        if (!collision.gameObject.CompareTag("Enemy") || !bCanBeTargeted)
         {
             return;
         }
--- 80s Game/Assets/Scripts/AI/Defendable/LatchPoint.cs
+++ 80s Game/Assets/Scripts/AI/Defendable/LatchPoint.cs
@@ -1,4 +1,3 @@
-using System.Threading;
 using UnityEngine;
 
 public class LatchPoint : MonoBehaviour
@@ -6,10 +5,20 @@ public class LatchPoint : MonoBehaviour
     public bool isAvailable;
     public Target latchedTarget;
 
+    // Latch a target to this point
     public void LatchTarget(Target target)
     {
         latchedTarget = target;
         target.transform.SetParent(transform);
         isAvailable = false;
+    }
+
+
+    // Release the latched target
+    public void Unlatch()
+    {
+        latchedTarget.transform.SetParent(null);
+        latchedTarget = null;
+        isAvailable = true;
     }
 }
