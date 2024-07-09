013b55009a0612cfae0f4566e1dd468174df717a
RitoxicGame <bigmagicrat@gmail.com>
2024-07-09 14:31:54 -0400

    Update AnimationHandler.cs
    modified code to reflect new guidelines
    removed obsolete commented code
    
    added attack hash and associated method

--- 80s Game/Assets/Scripts/AnimationHandler.cs
+++ 80s Game/Assets/Scripts/AnimationHandler.cs
@@ -5,76 +5,81 @@ using UnityEngine;
 public class AnimationHandler : MonoBehaviour
 {
     Animator anime; //animator for the given object
-    //Target self; //Target script for object
-    private int stunHash;
-    private int dropHash;
-    private int resetHash;
-    //private int timeHash = Animator.StringToHash("stunTime");
-    //private bool dying = false;
-    //private int deathTimer;
-    //private const int DTIME = 10;
+
+    private int _stunHash;
+    private int _dropHash;
+    private int _resetHash;
+    private int _attackHash;
 
     // Start is called before the first frame update
     void Awake()
     {
         anime = GetComponent<Animator>(); //set up animator
 
-        stunHash = Animator.StringToHash("hit");
-        dropHash = Animator.StringToHash("drop");
-        resetHash = Animator.StringToHash("reset");
-
-        //self = GetComponent<Target>();
-        //deathTimer = DTIME;
-        //anime.SetInteger(timeHash, deathTimer);
+        _stunHash = Animator.StringToHash("hit");
+        _dropHash = Animator.StringToHash("drop");
+        _resetHash = Animator.StringToHash("reset");
+        _attackHash = Animator.StringToHash("attack");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (dying != true && self.currentState == TargetStates.Death)
-        //{
-        //    dying = true;
-        //    anime.SetTrigger(stunHash);
-        //}
-        //if (dying == true && deathTimer > 0) //death animation should loop for ~10 frames
-        //{
-        //    deathTimer--;
-        //    anime.SetInteger(timeHash, deathTimer);
-        //}
-        //if (self.currentState != TargetStates.Death && dying == true) //target has been reset, needs to be updated
-        //{
-        //    dying = false;
-        //    deathTimer = DTIME;
-        //    anime.SetInteger(timeHash, deathTimer);
-        //    anime.ResetTrigger(stunHash);
-        //}
     }
 
+    /// <summary>
+    /// Sets the "stun" trigger for the animator, initiating the Stun animation
+    /// </summary>
     public void PlayStunAnimation()
     {
         // Reset potentially active animations
-        anime.ResetTrigger(dropHash);
-        anime.ResetTrigger(resetHash);
+        anime.ResetTrigger(_dropHash);
+        anime.ResetTrigger(_resetHash);
+        anime.SetTrigger(_attackHash);
 
         // Set new animation
-        anime.SetTrigger(stunHash);
+        anime.SetTrigger(_stunHash);
     }
+
+    /// <summary>
+    /// Sets the "drop" trigger for the animator, initiating the Drop animation
+    /// </summary>
     public void PlayDropAnimation()
     {
         // Reset potentially active animations
-        anime.ResetTrigger(stunHash);
-        anime.ResetTrigger(resetHash);
+        anime.ResetTrigger(_stunHash);
+        anime.ResetTrigger(_resetHash);
+        anime.SetTrigger(_attackHash);
 
         // Set new animation
-        anime.SetTrigger(dropHash);
+        anime.SetTrigger(_dropHash);
     }
+
+    /// <summary>
+    /// Sets the "attack" trigger for the animator, initiating the Attack animation
+    /// </summary>
+    public void PlayAttackAnimation()
+    {
+        // Reset potentially active animations
+        anime.ResetTrigger(_stunHash);
+        anime.ResetTrigger(_resetHash);
+        anime.ResetTrigger(_dropHash);
+
+        // Set new animation
+        anime.SetTrigger(_attackHash);
+    }
+
+    /// <summary>
+    /// Sets the "reset" trigger for the animator, resetting the animation state machine to the default Flight animation
+    /// </summary>
     public void ResetAnimation()
     {
         // Reset potentially active animations
-        anime.ResetTrigger(stunHash);
-        anime.ResetTrigger(dropHash);
+        anime.ResetTrigger(_stunHash);
+        anime.ResetTrigger(_dropHash);
+        anime.SetTrigger(_attackHash);
 
         // Set new animation
-        anime.SetTrigger(resetHash);
+        anime.SetTrigger(_resetHash);
     }
 }
