66239574c352ef72ec982cc1215ce55d9da466f3
Kevin-Insinna <kji8379@rit.edu>
2024-07-16 12:36:30 -0400

    Fixed highlight still being not lined up

--- 80s Game/Assets/Scripts/GameModes/ClassicMode.cs
+++ 80s Game/Assets/Scripts/GameModes/ClassicMode.cs
@@ -13,7 +13,7 @@ public class ClassicMode : AbsGameMode
         ModeType = EGameMode.Classic;
 
         // Initial round parameters
-        NumRounds = 10;
+        NumRounds = 11;
         maxTargetsOnScreen = 8;
         currentRoundTargetCount = 5;
 
