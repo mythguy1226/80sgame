3e37c65de219f71b58b11de5061bbe39c4c7ae08
Rob Reddick <robbiereddick@aol.com>
2024-07-22 11:35:13 -0400

    Started work on persistent BGM implementation

--- 80s Game/Assets/Scripts/UI Scripts/OnboardingUI.cs
+++ 80s Game/Assets/Scripts/UI Scripts/OnboardingUI.cs
@@ -12,8 +12,11 @@ public class OnboardingUI : MonoBehaviour
     public GameObject xboxControllerDiagram;
     public GameObject playstationControllerDiagram;
     [SerializeField] AudioClip gameStartTheme;
+    [SerializeField] AudioClip gameLoopIntro;
+    [SerializeField] AudioClip gameLoopBGM;
     private UIManager manager;
     private bool controllerConnected = false;
+    private bool playedBGM = false;
 
 
     // Start is called before the first frame update
@@ -49,10 +52,36 @@ public class OnboardingUI : MonoBehaviour
 
         Time.timeScale = 1.0f;
         manager.activeUI = UIManager.UIType.None;
+
+        // Start coroutine for playing BGM
+        StartCoroutine(DelayPersistentBGMIntro(gameStartTheme.length));
     }
 
     public void SetManager(UIManager reference)
     {
         manager = reference;
+    }
+
+    /// <summary>
+    /// Coroutine used for delaying play of BGM intro
+    /// </summary>
+    IEnumerator DelayPersistentBGMIntro(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SoundManager.Instance.PlaySoundContinuous(gameLoopIntro);
+        if(!playedBGM)
+        {
+            StartCoroutine(DelayPersistentBGM(gameLoopIntro.length));
+            playedBGM = true;
+        }
+    }
+
+    /// <summary>
+    /// Coroutine used for delaying play of persistent BGM
+    /// </summary>
+    IEnumerator DelayPersistentBGM(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SoundManager.Instance.SetMusicToLoop(gameLoopBGM);
     }
 }
